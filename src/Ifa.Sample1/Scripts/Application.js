3d5725b03518baff199293c8185ab328fc625903
Leonardo Cardoso <leonardo@buzzero.com>
2012-03-22 18:36:49 -0300

    App code.

---
+++ src/Ifa.Sample1/Scripts/Application.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $("#theme").live("change", function () {
+        var url = $(this).attr("data-url").replace("0", $(this).val());
+        $.get(url, function (data) {
+            $("#products").html(data);
+        });
+    });
+});
+
+function UpdateProductsTable(data, status, xhr) {
+    $("#products").html(data);
+}
